e8979fea22ca2f329c89ae5018c552fb044ee426
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-18 12:01:27 +0200

    Edit: added both constructors adn toString in the page class

--- src/model/Page.java
+++ src/model/Page.java
@@ -44,6 +44,19 @@ public class Page {
 		}
 	}
 	
+	//4.abi konstruktori
+	public Page() {
+		setTitle("Noklusejuma lapa");
+		setDescription("Lapa par neko");
+	}
 	
+	public Page(String inputTitle, String inputDescription) {
+		setTitle(inputTitle);
+		setDescription(inputDescription);
+	}
+	public String toString() {
+		String result = title + " " + description + "; sludinajumi\n ->" + allPostsInPage + "\n sekotaji: " + allPageFollowers;
+		return result;
+	}
 
 }
